92e954eae023a3aa0083e5386d2dd2c03e2b9b60
marquessv <marquessavaldez@gmail.com>
2024-05-18 09:45:06 -0700

    change user script

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR=~/.dotfiles
 nix-shell -p git --command "git clone https://github.com/marquessv/nixos-config $SCRIPT_DIR"
 sudo nixos-generate-config --show-hardware-config > $SCRIPT_DIR/system/hardware-configuration.nix
 sudo nixos-rebuild switch --extra-experimental-features flakes --flake $SCRIPT_DIR#system;
-nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#jefe;
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#user;
